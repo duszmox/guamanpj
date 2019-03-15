f6129bf9402dcdcb2754d3b99341d7c3186a7b3f
duszmox <38438001+duszmox@users.noreply.github.com>
2019-03-15 19:13:46 +0100

    favicon

--- guaman-project/application/views/templates/header.php
+++ guaman-project/application/views/templates/header.php
@@ -24,6 +24,14 @@ if (!isset($title)) {
 	<script src="<?php echo js_url("jquery.easing.min.js");?>" ></script>
 	<script src="<?php echo js_url("jquery.dataTables.min.js");?>" ></script>
 	<script src="<?php echo js_url("dataTables.bootstrap4.min.js");?>" ></script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="https://guamanpj.com/guaman-project/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://guamanpj.com/guaman-project/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://guamanpj.com/guaman-project/favicon/favicon-16x16.png">
+    <link rel="manifest" href="https://guamanpj.com/guaman-project/favicon/site.webmanifest">
+    <link rel="mask-icon" href="https://guamanpj.com/guaman-project/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">ł
 </head>
 <body>
 <div id="wrapper">
