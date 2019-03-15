4dd18615317fc67e6d0fc35541b93d86083530d1
duszmox <38438001+duszmox@users.noreply.github.com>
2019-03-15 18:04:14 +0100

    favicon logo

--- guaman-project/application/config/guaman_config.php
+++ guaman-project/application/config/guaman_config.php
@@ -7,7 +7,7 @@
  */
 $config["table_prefix"] = "guaman_";
 $config["website_title"] = "Guaman Projects";
-$config["website_logo_url"] = "https://guamanpj.com/guaman-project/assets/img/logo.jpg";
+$config["website_logo_url"] = "https://guamanpj.com/guaman-project/assets/img/logo.png";
 $config["allow_registration"] = false;
 
 $config["modules"] = array();
