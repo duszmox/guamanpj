ea6ad1abf491847d601a1eab74d8a9174d1be937
Ambrus <ambrus@ambrusweb11.hu>
2019-03-16 17:30:38 +0100

    nagyon meno folder view

--- guaman-project/application/views/database/table_view.php
+++ guaman-project/application/views/database/table_view.php
@@ -13,90 +13,13 @@
 			</div>
 		</div>
 	</div>
-	<script>
-		function openFolder(folder_name, current_folder) {
-			//todo jQuery: Nyissa ki a folder, minden childnak tegye a display-ét visible-vé none-ról. Emellett az onclick folder, az legyen closeFolder(), és a .html-jében pedig változtassa meg az ikont, <i class="fa fa-angle-up"></i>, visible, és invisible classok létrehozva
-			//todo minden táblához hozzá lett adva egy parentfoler_ tag, és a folder neve.
-			//todo a mappákat is tegye bele a parentfolderbe, és csak a MAIN maradjon kint
-		}
-
-		function closeFolder() {
-			//todo display = none, jQuery
-
-		}
-	</script>
-
 	<div class="col-sm-4">
 		<div class="card">
-			<div class="card-body">
-				<div class="table-responsive">
-					<table class="table table-bordered" width="100%" cellspacing="0">
-						<?php
-						/*
-						foreach ($table_array as $key => $table) {
-							echo "<tr>";
-							/*foreach ($table as $key => $value) {
-                                echo "<td>" . $value . "</td>";
-                            }
-							echo "<td class='parentfolder_" . $table["parent_folder"] . " 'onclick='loadTable(\"" . $table["table_name"] . "\")'>" .
-								"<i class='fas fa-database'></i> " . $table["table_title"] .
-								"</td>";
-							echo "</tr>";
-						}
-						foreach ($folder_array as $key => $folder) {
-							echo "<tr><div class='folder_" . $folder["folder_name"] . "'>\n";
-							/*foreach ($table as $key => $value) {
-                                echo "<td>" . $value . "</td>";
-                            }
-							echo "<td  onclick='openFolder(\"" . $folder["folder_name"] . "\"'>\n" .
-								"<i class='fas fa-folder'></i> " . $folder["folder_title"] . "  <i class=\"fa fa-angle-down\"></i>\n" .
-								"</td>\n";
-							echo "</div></tr>\n";
-						}*/
-
-
-						?>
-						<script>
-							var folders = <?php echo json_encode($folder_array); ?>;
-							var tables = <?php echo json_encode($table_array); ?>;
-
-							function hasThisAsParentFolder(needle, haystack) {
-								for (var i = 0; i < haystack.size; i++) {
-									if (needle === haystack[i].parent_folder) {
-										return true;
-									}
-								}
-								return false;
-							}
-
-							var output = "";
-
-							function tree(current_folder, level) {
-
-								for (var i = 0; i < level; i++) {
-									output += "-";
-								}
-								output += " " + current_folder;
-								// Ha a current_folder egy foldernek a parentje:
-								for (var i = 0; i < folders.length; i++) {
-									if (current_folder === folders[i].parent_folder) {
-										tree(folders[i].parent_folder, level + 1);
-									}
-								}
-
-								// Ha a current_folder egy table-nek a parentje:
-								/*for (var i = 0; i < folders.length; i++) {
-									if (current_folder === tables[i].parent_folder) {
-										tree(folders[i].parent_folder, level + 1);
-									}
-								}*/
-
-							}
-							//tree("", 1);
-							//console.log(output);
-						</script>
-					</table>
-				</div>
+			<div class="card-header">
+				<h2>Táblázatok:</h2>
+			</div>
+			<div class="card-body" id="table-list-container">
+				Ide fog betölteni a táblázatok listája
 			</div>
 		</div>
 	</div>
@@ -126,6 +49,70 @@
 	};
 
 	var base_url = "<?php echo base_url(); ?>";
+
+
+	var folders = <?php echo json_encode($folder_array); ?>;
+	var tables = <?php echo json_encode($table_array); ?>;
+
+	function hasThisAsParentFolder(needle, haystack) {
+		for (var i = 0; i < haystack.size; i++) {
+			if (needle === haystack[i].parent_folder) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	var output = "<ul id='table-list' class='jsl-open'>";
+
+	function get_folder_title_by_name(folder_name) {
+		for (var i = 0; i < folders.length; i++) {
+			if (folders[i].folder_name === folder_name) {
+				return folders[i].folder_title;
+			}
+		}
+		return "";
+	}
+
+	function tree(current_folder, level) {
+
+		output += "<li>";
+		output += '<i class="fa fa-folder-o" aria-hidden="true"></i>';
+		output += "<i class=\"fas fa-folder\"></i> <b>" + get_folder_title_by_name(current_folder) + "</b>";
+
+		// Ha a current_folder egy foldernek a parentje:
+		output += "<ul>";
+		for (var i = 0; i < folders.length; i++) {
+			if (current_folder === folders[i].parent_folder) {
+				tree(folders[i].folder_name, level + 1);
+			}
+		}
+
+
+		// Ha a current_folder egy table-nek a parentje:
+		for (var i = 0; i < tables.length; i++) {
+			if (current_folder === tables[i].parent_folder) {
+				output += '<li onclick="loadTable(\'' + tables[i].table_name + '\')"><i class="fa fa-file-text-o" aria-hidden="true"></i> <i class=\'fas fa-database\'></i> ' + tables[i].table_title + "</li>";
+			}
+		}
+
+		output += "</ul>";
+		output += "</li>";
+
+	}
+
+	tree("", 1);
+
+	output += "</ul>";
+	$("#table-list-container").html(output);
+
+	JSLists.applyToList('table-list', 'ALL');
 </script>
+
+<style>
+	#table-list-container ul {
+		list-style-type: none;
+	}
+</style>
 
 <script async src="<?php echo js_url("table_view.js"); ?>"></script>
--- guaman-project/application/views/templates/header.php
+++ guaman-project/application/views/templates/header.php
@@ -17,29 +17,30 @@ if (!isset($title)) {
 		href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext"
 		rel="stylesheet">
 	<script
-            src="https://code.jquery.com/jquery-3.3.1.min.js"
+		src="https://code.jquery.com/jquery-3.3.1.min.js"
 		integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 		crossorigin="anonymous"></script>
-	<script src="<?php echo js_url("bootstrap.bundle.min.js");?>" ></script>
-	<script src="<?php echo js_url("jquery.easing.min.js");?>" ></script>
-	<script src="<?php echo js_url("jquery.dataTables.min.js");?>" ></script>
-	<script src="<?php echo js_url("dataTables.bootstrap4.min.js");?>" ></script>
+	<script src="<?php echo js_url("bootstrap.bundle.min.js"); ?>"></script>
+	<script src="<?php echo js_url("jquery.easing.min.js"); ?>"></script>
+	<script src="<?php echo js_url("jquery.dataTables.min.js"); ?>"></script>
+	<script src="<?php echo js_url("dataTables.bootstrap4.min.js"); ?>"></script>
+	<script src="<?php echo js_url("jsLists.min.js"); ?>"></script>
 
-    <link rel="icon" href="<?php echo img_url("logo.png")?>" type="image/x-icon"/>
-    <link rel="shortcut icon" href="<?php echo img_url("logo.png")?>" type="image/x-icon"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
+	<link rel="icon" href="<?php echo img_url("logo.png") ?>" type="image/x-icon"/>
+	<link rel="shortcut icon" href="<?php echo img_url("logo.png") ?>" type="image/x-icon"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <link rel="apple-touch-icon" sizes="180x180" href="https://guamanpj.com/guaman-project/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://guamanpj.com/guaman-project/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://guamanpj.com/guaman-project/favicon/favicon-16x16.png">
-    <link rel="manifest" href="https://guamanpj.com/guaman-project/favicon/site.webmanifest">
-    <link rel="mask-icon" href="https://guamanpj.com/guaman-project/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
+	<link rel="apple-touch-icon" sizes="180x180"
+		  href="https://guamanpj.com/guaman-project/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32"
+		  href="https://guamanpj.com/guaman-project/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16"
+		  href="https://guamanpj.com/guaman-project/favicon/favicon-16x16.png">
+	<link rel="manifest" href="https://guamanpj.com/guaman-project/favicon/site.webmanifest">
+	<link rel="mask-icon" href="https://guamanpj.com/guaman-project/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">
 
 
 </head>
