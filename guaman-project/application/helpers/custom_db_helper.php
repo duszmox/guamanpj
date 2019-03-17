ca5f524196a2ad44322033637fc395c1db24fd7d
horvathandrasmate <Ocsikem2004>
2019-03-17 23:43:51 +0100

    custom_db_helper add tables

--- guaman-project/application/helpers/custom_db_helper.php
+++ guaman-project/application/helpers/custom_db_helper.php
@@ -8,17 +8,60 @@
 function custom_db_actions($table_name, $result_array, $column_names)
 {
 
-	switch ($table_name) {
-		case "guaman_social":
-			foreach ($result_array as $key => $row) {
-				// update $row["bevetel"] ;
-			}
+    $result_array_ = $result_array;
 
-			break;
+    switch ($table_name) {
+        case "guaman_keresesilista":
+            foreach ($result_array_ as $key => $row) {
+                $result_array_[$key]['kozepar'] = ((float)$result_array_[$key]['indulo_ar'] + (float)$result_array_[$key]['vegso_ar']) / 2;
+                $result_array_[$key]['celar'] = (float)$result_array_[$key]['kozepar'] * 0.8;
+
+            }
+            break;
+        case "guaman_keszlet":
+            foreach ($result_array_ as $key => $row) {
+                $result_array_[$key]['afa'] = round((float)$result_array_[$key]['eladasi_ar'] - (float)$result_array_[$key]['beszerzesi_ar']) / 1.27;
+                $result_array_[$key]['netto_profit'] = round(((float)$result_array_[$key]['eladasi_ar'] - (float)$result_array_[$key]['beszerzesi_ar']) - (((float)$result_array_[$key]['eladasi_ar'] - (float)$result_array_[$key]['beszerzesi_ar']) / 1.27));
+                if ((float)$result_array_[$key]['eladasi_ar'] != 0){
+                    $result_array_[$key]['%'] = round($result_array_[$key]['netto_profit'] / (float)$result_array_[$key]['eladasi_ar']);
+                };
+            }
+            break;
+        case "guaman_telefon":
+            foreach ($result_array_ as $key => $row) {
+                $netto_profit = $result_array_[$key]['kiker_brutto'] - $result_array_[$key]['bek_brutto'];
+                $netto_ = $netto_profit / $result_array_[$key]['kiker_brutto'];
+                $result_array_[$key]['netto_profit'] = $netto_profit;
+                $result_array_[$key]['netto_%'] = $netto_;
+            }
+            break;
+        case "guaman_gadget":
+            foreach ($result_array_ as $key => $row) {
+                $netto_profit = $result_array_[$key]['kiker_brutto'] - $result_array_[$key]['bek_brutto'];
+                $netto_ = $netto_profit / $result_array_[$key]['kiker_brutto'];
+                $result_array_[$key]['netto_profit'] = $netto_profit;
+                $result_array_[$key]['netto_%'] = $netto_;
+            }
+            break;
+        case "guaman_orakkiegeszitok":
+            foreach ($result_array_ as $key => $row) {
+                $netto_profit = $result_array_[$key]['kiker_brutto'] - $result_array_[$key]['bek_brutto'];
+                $netto_ = $netto_profit / $result_array_[$key]['kiker_brutto'];
+                $result_array_[$key]['netto_profit'] = $netto_profit;
+                $result_array_[$key]['netto_%'] = $netto_;
+            }
+            break;
+        case "guaman_tablet":
+            foreach ($result_array_ as $key => $row) {
+                $netto_profit = $result_array_[$key]['kiker_brutto'] - $result_array_[$key]['bek_brutto'];
+                $netto_ = $netto_profit / $result_array_[$key]['kiker_brutto'];
+                $result_array_[$key]['netto_profit'] = $netto_profit;
+                $result_array_[$key]['netto_%'] = $netto_;
+            }
+            break;
+
+    }
 
 
-	}
-
-
-	return $result_array;
+    return $result_array_;
 }
