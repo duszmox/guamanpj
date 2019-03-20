89b243f2b2ab67b73adfb687f63698f0e8901e3b
Ambrus Tóth <ambrus@ambrusweb11.hu>
2019-03-20 20:20:57 +0100

    english

--- guaman-project/application/language/english/information_lang.php
+++ guaman-project/application/language/english/information_lang.php
@@ -1,8 +1,82 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: horva
+ * User: Ambrus Toth
  * Date: 2019.03.14.
- * Time: 19:17
+ * Time: 03:19
  */
-$lang["hello"] = "Hellooo";
+
+$lang["enter_valid_data_message"] = "Enter valid data!";
+$lang["successful_login_message"] = "You have succesfully logged in!";
+$lang["my_profile_title"] = "My profile";
+$lang["logout_message"] = "Succesfully logged out!";
+
+$lang["rank_logged_out"] = "logged out";
+$lang["rank_logged_in"] = "logged in";
+$lang["rank_admin"] = "admin";
+$lang["login_title"] = "Log in";
+$lang["wrong_login_message"] = "Wrong username / password!";
+$lang["used_username_message"] = "This username has already taken!";
+$lang["used_email_message"] = "This email adress has already registered!";
+$lang["successful_registration_message"] = "Succesfull registration!";
+$lang["registration_title"] = "Registration";
+$lang["language_label"] = "Choose a language";
+$lang["logout_label"] = "Log out";
+$lang["username_placeholder"] = "Username";
+$lang["password_placeholder"] = "Password";
+$lang["login_message"] = "Log in";
+$lang["send_login_message"] = "Send";
+$lang["create_account_message"] = "Create an account!";
+$lang["already_have_an_account_message"] = "Already have an account? Log in!";
+$lang["send_registration_message"] = "Register";
+$lang["email_placeholder"] = "Email address";
+$lang["create_account_message"] = "Create an account!";
+$lang["profile_settings"] = "Settings";
+$lang["profile_label"] = "Profile";
+$lang["settings_label"] = "Settings";
+$lang["settings_page_title"] = "Settings";
+$lang["modules_title"] = "Modules";
+$lang["database_system_title"] = "Database";
+$lang["statistics_label"] = "Statistics";
+$lang["tables_title"] = "Tables";
+$lang["profile_title"] = "Profile";
+$lang["settings_title"] = "Settings";
+$lang["shown_pages_start_message"] = "";
+$lang["shown_pages_end_message"] = " row / page";
+$lang["searcher_message"] = "Searching: ";
+$lang["processing_message"] = "Processing...";
+$lang["first_page_message"] = "First";
+$lang["previous_page_message"] = "Previous";
+$lang["last_page_message"] = "Last";
+$lang["next_page_message"] = "Next";
+$lang["asc_table_message"] = ": ascending order";
+$lang["desc_table_message"] = ": descending order";
+$lang["records_loading_message"] = "Loading items...";
+$lang["records_zero_message"] = "There are no matching items.";
+$lang["empty_table_message"] = "Empty table.";
+$lang["create_folder_title"] = "Create new folder";
+$lang["folder_name_title"] = "Folders's name: ";
+$lang["folder_destination_title"] = "Parent folder: ";
+$lang["create_table_title"] = "Create new table ";
+$lang["insert_row_title"] = "Insert new line";
+$lang["current_password_label"] = "Current password:";
+$lang["new_password_label"] = "New password:";
+$lang["again_password_label"] = "New password again:";
+$lang["change_password_title"] = "Change password";
+$lang["submit_change_password_button"] = "Change";
+$lang["invalid_current_password"] = "Invalid password. Try again!";
+$lang["again_password_not_matching"] = "The passwords don't match.";
+$lang["invalid_new_password"] = "The new password contains invalid characters. Valid characters are: numbers, letters, _, and -.";
+$lang["successful_password_changing"] = "Password changed succesfully. Log in to continue!";
+$lang["username_label"] = "Username: ";
+$lang["email_label"] = "Email adress: ";
+$lang["password_label"] = "Password: ";
+$lang["rank_label"] = "Role: ";
+$lang["phone_number_label"] = "Phone number: ";
+$lang["change_password_button"] = "Change password";
+$lang["public"] = "Public";
+$lang["tables_label"] = "Tables: ";
+$lang["not_having_permission_message"] = "You don't have permission to visit this page!";
+$lang["table_body_placeholder"] = "The table...";
+
+$lang["manage_permissions_title"] = "Permissions";
+$lang["admin_menu_title"] = "Admin site";
