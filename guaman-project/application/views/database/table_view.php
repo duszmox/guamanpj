4257afabf1c8b31a9471e0116030cac0747d0c45
duszmo <duszmox@gmail.com>
2019-05-04 21:57:13 +0200

    Hide Show button is working lol :D

--- guaman-project/application/views/database/table_view.php
+++ guaman-project/application/views/database/table_view.php
@@ -1,73 +1,86 @@
 <div class="row">
-	<div class="col-sm-8" style="visibility: hidden;" id="data-table-column">
-		<div class="card">
-			<div class="card-body">
+    <div class="col-sm-8" style="visibility: hidden;" id="data-table-column">
+        <div class="card">
+            <div class="card-body">
 
-				<div class="responsive-table" id="table-container">
+                <div class="responsive-table" id="table-container">
                     <!--h3 class="align-center"><?php echo lang("table_body_placeholder"); ?></h3-->
-				</div>
+                </div>
 
-			</div>
-		</div>
-	</div>
-	<div class="col-sm-4">
-		<div class="card">
-			<div class="card-header">
-				<h2 class="d-inline"><?php echo lang("tables_label"); ?></h2>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="card">
+            <div class="card-header">
+                <h2 class="d-inline"><?php echo lang("tables_label"); ?></h2>
 
-               <div class="hideshowbtn d-inline">
-                   <form class="d-inline" action="" method="post">
-                       <input name="zero" class="btn btn-primary d-inline" style="margin-bottom: 10px; float: right;"  type="button" value="Hide" onclick="return change(this); <?php echo "lol";?>" />
-                   </form>
-                   <script>
-                       function change( el )
-                       {
-                           if ( el.value === "<?php echo lang("tables_button_hide")?>" )
-                               el.value = "<?php echo lang("tables_button_show")?>";
+                <div class="hideshowbtn d-inline">
+                    <form class="d-inline" action="" method="post">
+                        <input name="zero" class="btn btn-primary d-inline" style="margin-bottom: 10px; float: right;"
+                               type="button" value="Hide" onclick="return change(this)"/>
+                    </form>
+                    <script>
+                        function change(el) {
+                            if (el.value === "<?php echo lang("tables_button_hide")?>") {
+                                el.value = "<?php echo lang("tables_button_show")?>";
+                                $("div.tables-container").toggle("fast", function(){
 
-                           else
-                               el.value = "<?php echo lang("tables_button_hide")?>";
-                       }
-                   </script>
+                                );
 
-               </div>
+                            }
+                        else{
+                            el.value = "<?php echo lang("tables_button_hide")?>";
+                            $("div.tables-container").toggle("fast", function(){
+
+                            });
+                        }
+                        }
+                    </script>
+
+                </div>
 
 
-			</div>
-			<div class="card-body" id="table-list-container">
-				..
+            </div>
+            <div class="card-body tables-container" id="table-list-container">
+                ..
                 .
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 <script>
-	var data_table_strings = {
-		processing: "<?php echo lang("processing_message");?>",
-		search: "<?php echo lang("searcher_message");?>",
-		lengthMenu: "<?php echo lang("shown_pages_start_message") . "_MENU_" . lang("shown_pages_end_message");?>",
-		info: "",
-		infoEmpty: "",
-		infoFiltered: "",
-		infoPostFix: "",
-		loadingRecords: "<?php echo lang("records_loading_message");?>",
-		zeroRecords: "<?php echo lang("records_zero_message");?>",
-		emptyTable: "<?php echo lang("empty_table_message");?>",
-		paginate: {
-			first: "<?php echo lang("first_page_message");?>",
-			previous: "<?php echo lang("previous_page_message");?>",
-			next: "<?php echo lang("next_page_message");?>",
-			last: "<?php echo lang("last_page_message");?>"
-		},
-		aria: {
-			sortAscending: "<?php echo lang("asc_table_message");?>",
-			sortDescending: "<?php echo lang("desc_table_message");?>"
-		}
-	};
+    var data_table_strings = {
+        processing: "<?php echo lang("processing_message");?>",
+        search: "<?php echo lang("searcher_message");?>",
+        lengthMenu: "<?php echo lang("shown_pages_start_message") . "_MENU_" . lang("shown_pages_end_message");?>",
+        info: "",
+        infoEmpty: "",
+        infoFiltered: "",
+        infoPostFix: "",
+        loadingRecords: "<?php echo lang("records_loading_message");?>",
+        zeroRecords: "<?php echo lang("records_zero_message");?>",
+        emptyTable: "<?php echo lang("empty_table_message");?>",
+        paginate: {
+            first: "<?php echo lang("first_page_message");?>",
+            previous: "<?php echo lang("previous_page_message");?>",
+            next: "<?php echo lang("next_page_message");?>",
+            last: "<?php echo lang("last_page_message");?>"
+        },
+        aria: {
+            sortAscending: "<?php echo lang("asc_table_message");?>",
+            sortDescending: "<?php echo lang("desc_table_message");?>"
+        }
+    };
 
-	var lang = {"reload_page_button": "<?php echo lang("reload_page_button_title");?>", "new_row_button": "<?php echo lang("add_row_button_title");?>", "move_row_button": "<?php echo lang("move_row_title");?>", "actions": "<?php echo lang("actions_button_title");?>"};
+    var lang = {
+        "reload_page_button": "<?php echo lang("reload_page_button_title");?>",
+        "new_row_button": "<?php echo lang("add_row_button_title");?>",
+        "move_row_button": "<?php echo lang("move_row_title");?>",
+        "actions": "<?php echo lang("actions_button_title");?>"
+    };
 
-	var base_url = "<?php echo base_url(); ?>";
+    var base_url = "<?php echo base_url(); ?>";
 
 
     var folders = <?php /** @var array $folder_array */
@@ -98,5 +111,9 @@
         margin-left: 1em;
         padding-left: 1em;
         list-style-position: inside;
+    }
+    .tables-container{
+        display:inline-block;
+
     }
 </style>
