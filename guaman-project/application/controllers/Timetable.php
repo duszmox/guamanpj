2e596bc0518b92acaa332ce9f70f9ca3e00cf902
horvathandrasmate <horvathandrasmate@gmail.com>
2019-05-10 21:37:32 +0200

    21:36 commit Placed at Gyula's

--- guaman-project/application/controllers/Timetable.php
+++ guaman-project/application/controllers/Timetable.php
@@ -16,29 +16,37 @@ class Timetable extends CI_Controller
 
         $this->load->model("Timetable_model");
 
+        $this->load->model("Account_model");
+
     }
 
     /**
-     * @param string $user
+     * @param int $user_id
      */
-    public function index($user = "")
+    public function index($user_id = 0)
     {
-        $this->load->model("Account_model");
-        /** @var string $user_id */
-        $user = $this->Account_model::$user_id;
-
+        if ($user_id == 0) {
+            //todo csak akkor ha van a HR modulból rá engedélye / főnöke
+            $user_id = $this->Account_model::$user_id;
+        }
         $this->load->view("templates/header", array("page_title" => lang("timetable_label")));
         $this->load->view("templates/menu");
-        $events_array = $this->Timetable_model->get_events_by_user($user);
-
-        foreach ($events_array as $key => $value) {
-            $var = $this->Timetable_model->get_event_title_by_id($value['event_id']);
-
-            $events_array[$key]['event_title'] = $var[0]['event_title'];
-
-            continue;
+        $events_array = $this->Timetable_model->get_events_by_user($user_id);
+        $empty = false;
+        if(empty($events_array)){
+            $events_array = array(array());
+            $empty = true;
         }
+        if(!$empty) {
 
+            foreach ($events_array as $key => $value) {
+                $var = $this->Timetable_model->get_event_title_by_id($value['event_id']);
+
+                $events_array[$key]['event_title'] = $var[0]['event_title'];
+
+                continue;
+            }
+        }
         $this->load->view("timetable/timetable", array("events_array" => $events_array));
 
 
@@ -58,7 +66,6 @@ class Timetable extends CI_Controller
                 js_alert("Invalid event id", base_url("timetable/"));
             }
         }
-
 
 
         $this->load->view("templates/header", array('page_title' => "Manage Event"));
