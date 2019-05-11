18e99e901c47a91f30c03e70bd62baa97b3a3c20
duszmox <duszmox@gmail.com>
2019-05-11 10:56:58 +0200

    Home.php

--- guaman-project/application/views/templates/home.php
+++ guaman-project/application/views/templates/home.php
@@ -1,5 +1,58 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<style>
+
+    h2.title     {
+        margin-left: 10%;
+        margin-bottom: 65px;
+        margin-top: 60px;
+    }
+    .statistics-add-card {
+        margin-top: 40px;
+    }
+    h3.patch-title {
+        margin-left: 3%;
+        margin-right: 3%;
+        margin-top: 20px;
+        border-bottom: 1px solid gray;
+
+    }
+    .patch-text {
+        word-wrap: break-word;
+        white-space: -moz-pre-wrap;
+        white-space: pre-wrap;
+        margin-left: 4%;
+        margin-right: 4%;
+    }
+    a.more {
+        float: right;
+    }
+    .card-footer {
+        background-color: white;
+    }
+</style>
+<body>
+<h2 class="title">What's New?</h2>
+<div class="card statistics-add-card container">
+    <h3 class="patch-title">V.1.0.1</h3>
+    <p class="patch-text">
+        KukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukikKukimukik....
+    </p>
+    <div class="card-footer">
+        <a class="btn btn-primary more" href="#" role="button">More..</a>
+
+    </div>
+</div>
 
 
-
-?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
